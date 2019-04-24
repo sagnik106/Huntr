62fe2ce10651e0a7a682524ec58eba897722a45e
sagnik106 <43110660+sagnik106@users.noreply.github.com>
2019-04-24 23:10:51 +0530

    Build complete

--- build/build.ino
+++ build/build.ino
@@ -1,11 +1,64 @@
+int c1=2;//channel 1
+int c2=4;//channel 2
+int dir1 = 12;
+int dir2 = 13;
+int pwm1 = 3;
+int pwm2 = 5;
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(c1,INPUT);
+  pinMode(c2,INPUT);
+  pinMode(dir1,OUTPUT);
+  pinMode(dir2,OUTPUT);
+  pinMode(pwm1,OUTPUT);
+  pinMode(pwm2,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-digitalWrite(5,HIGH);
-analogWrite(6,100);
+int ch1=pulseIn(c1,HIGH);
+int ch2=pulseIn(c2,HIGH);
+
+  Serial.println(ch1);
+ch1=map(ch1,1190,1890,-255,255);
+ch2=map(ch2,1650,1300,-255,255);
+if(ch2>12)
+forward(ch2);
+else if(ch2<-21)
+backward(255-ch2);
+else{
+  if(ch1<-18)
+  left(255-ch1);
+  else if(ch1>18)
+  right(ch1);
+  else
+  forward(0);
+}
+}
+void forward(int pwm)
+{
+  digitalWrite(dir1,LOW);
+  analogWrite(pwm1,pwm);
+  digitalWrite(dir2,LOW);
+  analogWrite(pwm2,pwm);
+}
+void backward(int pwm)
+{
+  digitalWrite(dir1,HIGH);
+  analogWrite(pwm1,pwm);
+  digitalWrite(dir2,HIGH);
+  analogWrite(pwm2,pwm);  
+}
+void left(int pwm)
+{
+  digitalWrite(dir2,HIGH);
+  analogWrite(pwm1,pwm);
+  digitalWrite(dir1,LOW);
+  analogWrite(pwm2,pwm); 
+}
+void right(int pwm)
+{
+  digitalWrite(dir1,HIGH);
+  analogWrite(pwm1,pwm);
+  digitalWrite(dir2,LOW);
+  analogWrite(pwm2,pwm);
 }
