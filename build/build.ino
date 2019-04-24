1603d8f37534ab0a8f5ecb13dbf3fe10409fb06a
sagnik106 <43110660+sagnik106@users.noreply.github.com>
2019-04-24 23:15:23 +0530

    test pass

--- build/build.ino
+++ build/build.ino
@@ -1,10 +1,11 @@
 int c1=2;//channel 1
 int c2=4;//channel 2
-int dir1 = 12;
-int dir2 = 13;
-int pwm1 = 3;
-int pwm2 = 5;
-void setup() {
+int dir1 = 12;//direction control motor 1
+int dir2 = 13;//direction control motor 2
+int pwm1 = 3;//speed contol motor 1
+int pwm2 = 5;//speed control motor 2
+void setup() 
+{
   Serial.begin(9600);
   pinMode(c1,INPUT);
   pinMode(c2,INPUT);
@@ -14,25 +15,26 @@ void setup() {
   pinMode(pwm2,OUTPUT);
 }
 
-void loop() {
-int ch1=pulseIn(c1,HIGH);
-int ch2=pulseIn(c2,HIGH);
-
+void loop() 
+{
+  int ch1=pulseIn(c1,HIGH);
+  int ch2=pulseIn(c2,HIGH);
   Serial.println(ch1);
-ch1=map(ch1,1190,1890,-255,255);
-ch2=map(ch2,1650,1300,-255,255);
-if(ch2>12)
-forward(ch2);
-else if(ch2<-21)
-backward(255-ch2);
-else{
-  if(ch1<-18)
-  left(255-ch1);
-  else if(ch1>18)
-  right(ch1);
+  ch1=map(ch1,1190,1890,-255,255);
+  ch2=map(ch2,1650,1300,-255,255);
+  if(ch2>12)
+    forward(ch2);
+  else if(ch2<-21)
+    backward(255-ch2);
   else
-  forward(0);
-}
+  {
+    if(ch1<-18)
+      left(255-ch1);
+    else if(ch1>18)
+      right(ch1);
+    else
+      forward(0);
+  }
 }
 void forward(int pwm)
 {
